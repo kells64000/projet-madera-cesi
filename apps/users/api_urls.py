034e0cce259bd8f49102be552fa4cb828eb49695
oriigamii <mistrnbdy@protonmail.com>
2019-02-11 09:57:20 +0100

    api all users on url /api/users

--- apps/users/api_urls.py
+++ apps/users/api_urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import api_views
 
 urlpatterns = [
-    path('user/', api_views.user_list),
+    path('users/', api_views.user_list),
     path('user/<int:pk>', api_views.user_detail)
 ]
