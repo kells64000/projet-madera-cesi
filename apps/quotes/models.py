634c063376d5256fdb2ec6b20660e20d6e4f8541
kells <kelian.bousquet@laposte.net>
2019-02-27 21:41:46 +0100

    maj quotes pour client et commerciaux

--- apps/quotes/models.py
+++ apps/quotes/models.py
@@ -1,13 +1,27 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from users.models import MaderaUser
+
+
 
 
 class Quote(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=30)
-    phone = models.CharField(max_length=10)
-    email = models.EmailField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     state = models.TextField()
     attachment = models.TextField(null=True, blank=True, default=None)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    # ForeignKeys
+    customer = models.ForeignKey(MaderaUser,
+                                 blank=False,
+                                 default=None,
+                                 null=False,
+                                 on_delete=models.DO_NOTHING,
+                                 related_name=_('customer'))
+    salesman = models.ForeignKey(MaderaUser,
+                                 blank=False,
+                                 default=None,
+                                 null=False,
+                                 on_delete=models.DO_NOTHING,
+                                 related_name=_('salesman'))
--- apps/quotes/serializers.py
+++ apps/quotes/serializers.py
@@ -5,18 +5,17 @@ from .models import Quote
 class QuoteListSerializer(serializers.ModelSerializer):
 
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=30)
-    phone = serializers.CharField(max_length=10)
-    email = serializers.EmailField()
     price = serializers.DecimalField(max_digits=10, decimal_places=2)
     state = serializers.CharField()
     attachment = serializers.CharField(allow_null=True, allow_blank=True)
     created_at = serializers.DateTimeField()
     updated_at = serializers.DateTimeField()
+    customer = serializers.SerializerMethodField()
+    salesman = serializers.SerializerMethodField()
 
     class Meta:
         model = Quote
-        fields = ('id', 'name', 'phone', 'email', 'price', 'state',
+        fields = ('id', 'customer', 'salesman' 'price', 'state',
                   'attachment', 'created_at', 'updated_at')
 
     def create(self, validated_data):
@@ -29,9 +28,8 @@ class QuoteListSerializer(serializers.ModelSerializer):
         """
         Update and return an existing `Snippet` instance, given the validated data.
         """
-        instance.name = validated_data.get('name', instance.name)
-        instance.phone = validated_data.get('phone', instance.phone)
-        instance.email = validated_data.get('email', instance.email)
+        instance.customer = validated_data.get('customer', instance.customer)
+        instance.salesman = validated_data.get('salesman', instance.salesman)
         instance.price = validated_data.get('price', instance.price)
         instance.state = validated_data.get('state', instance.state)
         instance.attachment = validated_data.get('attachment', instance.attachment)
