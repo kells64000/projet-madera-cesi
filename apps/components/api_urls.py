27cc5597ae8642650e7649fafac69d340d92e665
oriigamii <mistrnbdy@protonmail.com>
2019-03-02 23:56:21 +0100

    update component api

--- apps/components/api_urls.py
+++ apps/components/api_urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import api_views
 
 urlpatterns = [
-    path('components/', api_views.components_list),
-    path('components/<int:pk>', api_views.components_detail)
+    path('components/', api_views.ListComponent.as_view(), name='components'),
+    path('components/<int:pk>', api_views.DetailComponent.as_view(), name='component')
 ]
--- apps/components/serializers.py
+++ apps/components/serializers.py
@@ -21,25 +21,6 @@ class ComponentSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'nature', 'length', 'width', 'depth', 'unit', 'surface',
                   'designer')
 
-    def create(self, validated_data):
-        """
-        Create and return a new `Component` instance, given the validated data.
-        """
-        return Component.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
-        instance.name = validated_data.get('name', instance.name)
-        instance.nature = validated_data.get('nature', instance.nature)
-        instance.length = validated_data.get('length', instance.length)
-        instance.width = validated_data.get('width', instance.width)
-        instance.depth = validated_data.get('depth', instance.depth)
-        instance.unit = validated_data.get('unit', instance.unit)
-        instance.save()
-        return instance
-
     def get_surface(self, obj):
         return obj.surface
 
