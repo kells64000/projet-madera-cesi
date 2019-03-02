5fd534ebe73511a42b116a788a1d86b5ffd64488
oriigamii <mistrnbdy@protonmail.com>
2019-03-02 22:31:19 +0100

    fix address serializer

--- apps/addresses/serializers.py
+++ apps/addresses/serializers.py
@@ -2,7 +2,28 @@ from rest_framework import serializers
 from .models import Address
 
 
-class AddressSerializer(serializers.ModelSerializer):
+class DynamicFieldsModelSerializer(serializers.ModelSerializer):
+    """
+    A ModelSerializer that takes an additional `fields` argument that
+    controls which fields should be displayed.
+    """
+
+    def __init__(self, *args, **kwargs):
+        # Don't pass the 'fields' arg up to the superclass
+        fields = kwargs.pop('fields', None)
+
+        # Instantiate the superclass normally
+        super(DynamicFieldsModelSerializer, self).__init__(*args, **kwargs)
+
+        if fields is not None:
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
+
+class AddressSerializer(DynamicFieldsModelSerializer):
     id = serializers.IntegerField(read_only=True)
     street = serializers.CharField(required=True)
     zipcode = serializers.CharField(required=False, allow_blank=True, max_length=8)
@@ -11,23 +32,4 @@ class AddressSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Address
-        fields = ('street', 'zipcode', 'city', 'country')
-        exclude = ('latitude', 'longitude')
-
-    def create(self, validated_data):
-        """
-        Create and return a new `MaderaUser` instance, given the validated data.
-        """
-        return Address.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
-        instance.street = validated_data.get('email', instance.email)
-        instance.zipcode = validated_data.get('first_name', instance.first_name)
-        instance.city = validated_data.get('last_name', instance.last_name)
-        instance.country = validated_data.get('address', instance.address)
-
-        instance.save()
-        return instance
+        fields = ('id', 'street', 'zipcode', 'city', 'country')
