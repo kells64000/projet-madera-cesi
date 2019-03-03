f26259c0651f76f30bfc44c3919bacb67c108e0f
oriigamii <mistrnbdy@protonmail.com>
2019-03-03 18:05:19 +0100

    fix address display quote finish

--- assets/js/components/stepCreateQuote/Recapitulatif.vue
+++ assets/js/components/stepCreateQuote/Recapitulatif.vue
@@ -6,8 +6,8 @@
             </div>
             <div class="card-header-title d-flex flex-column align-end">
                     <div>{{quoteClient.fullName}}</div>
-                    <div>{{quoteClient.address}}</div>
-                    <div>{{quoteClient.postalCode}} {{quoteClient.city}}</div>
+                    <div>{{quoteClient.address['street']}}</div>
+                    <div>{{quoteClient.address['zipcode']}} {{quoteClient.address['city']}}</div>
                     <div>{{quoteClient.email}}</div>
                     <div>{{quoteClient.phone}}</div>
             </div>
