4b7145fae346661b6fec1340d64c099252f88e02
oriigamii <mistrnbdy@protonmail.com>
2019-03-05 01:09:45 +0100

    update quotes serializer

--- apps/quotes/api_urls.py
+++ apps/quotes/api_urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import api_views
 
 urlpatterns = [
-    path('quotes/', api_views.quotes_list),
-    path('quotes/<int:pk>', api_views.quotes_detail)
+    path('quotes/', api_views.ListQuote.as_view(), name='quotes'),
+    path('quotes/<int:pk>', api_views.DetailQuote.as_view(), name='quote'),
+    path('quotes/<int:pk>/pdf', api_views.DetailQuote.as_view(), name='quote2pdf')
 ]
--- apps/quotes/serializers.py
+++ apps/quotes/serializers.py
@@ -1,37 +1,49 @@
 from rest_framework import serializers
+from users.serializers import ClientSerializer, SalesPersonSerializer
+
+from users.models import Client, SalesPerson
 from .models import Quote
 
 
-class QuoteListSerializer(serializers.ModelSerializer):
+class QuoteSerializer(serializers.ModelSerializer):
 
     id = serializers.IntegerField(read_only=True)
     price = serializers.DecimalField(max_digits=10, decimal_places=2)
-    state = serializers.CharField()
-    attachment = serializers.CharField(allow_null=True, allow_blank=True)
-    created_at = serializers.DateTimeField()
-    updated_at = serializers.DateTimeField()
-    customer = serializers.SerializerMethodField()
-    salesman = serializers.SerializerMethodField()
+    state = serializers.CharField(required=False, default='Brouillon')
+    attachment = serializers.CharField(required=False, allow_null=True, allow_blank=True)
+    client = ClientSerializer(required=False)
+    salesperson = SalesPersonSerializer(required=False)
+
+    created_at = serializers.DateTimeField(required=False)
+    updated_at = serializers.DateTimeField(required=False)
 
     class Meta:
         model = Quote
-        fields = ('id', 'customer', 'salesman' 'price', 'state',
-                  'attachment', 'created_at', 'updated_at')
-
-    def create(self, validated_data):
-        """
-        Create and return a new `Quote` instance, given the validated data.
-        """
-        return Quote.objects.create(**validated_data)
+        fields = ('id', 'price', 'state', 'attachment', 'client', 'salesperson',
+                  'created_at', 'updated_at')
 
     def update(self, instance, validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
-        instance.customer = validated_data.get('customer', instance.customer)
-        instance.salesman = validated_data.get('salesman', instance.salesman)
-        instance.price = validated_data.get('price', instance.price)
-        instance.state = validated_data.get('state', instance.state)
-        instance.attachment = validated_data.get('attachment', instance.attachment)
+        instance.email = validated_data.get('email', instance.email)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.phone = validated_data.get('phone', instance.phone)
+
+        if instance.client:
+            for k, v in validated_data.get('client').items():
+                instance.client.__dict__[k] = v
+            instance.client.save(update_fields=validated_data.get('client').keys())
+        else:
+            address_data = validated_data.get('client')
+            instance.client = Client.objects.create(**address_data)
+            instance.client.save()
+
+        if instance.salesperson:
+            for k, v in validated_data.get('salesperson').items():
+                instance.salesperson.__dict__[k] = v
+            instance.salesperson.save(update_fields=validated_data.get('salesperson').keys())
+        else:
+            address_data = validated_data.get('salesperson')
+            instance.salesperson = SalesPerson.objects.create(**address_data)
+            instance.salesperson.save()
         instance.save()
         return instance
