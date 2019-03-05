0eec4c006eb69403df4ace2e4ed30fc4cd2d9952
oriigamii <mistrnbdy@protonmail.com>
2019-03-05 01:08:36 +0100

    add module and gamme class

--- apps/components/models.py
+++ apps/components/models.py
@@ -17,14 +17,6 @@ class Component(models.Model):
     unit = models.CharField(_('unit'), max_length=10, blank=False, null=False)
     price = models.DecimalField(_('price'), max_digits=10, decimal_places=2, null=True)
 
-    # ForeignKeys
-    designer = models.ForeignKey(MaderaUser,
-                                 blank=True,
-                                 default=None,
-                                 null=True,
-                                 on_delete=models.DO_NOTHING,
-                                 related_name=_('designer'))
-
     @property
     def surface(self):
         return (self.length * self.width * (self.depth if self.depth else 1)
@@ -35,8 +27,24 @@ class Component(models.Model):
                                                                    l=self.length,
                                                                    w=self.width)
 
+
+class Module(Component):
+
+    components = models.ManyToManyField(Component, related_name='material')
+    designer = models.ForeignKey(MaderaUser,
+                                 blank=True,
+                                 default=None,
+                                 null=True,
+                                 on_delete=models.DO_NOTHING,
+                                 related_name=_('designer'))
+
     @property
     def designed_by(self):
         if self.designer:
             return self.designer.get_full_name()
         return None
+
+
+class Gamme(Component):
+
+    components = models.ManyToManyField(Component, related_name='finishing')
