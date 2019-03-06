3b5a3b093b9a2262107229b655f8b705767e8a17
oriigamii <mistrnbdy@protonmail.com>
2019-03-07 00:35:21 +0100

    fix admin user creation

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,7 +1,10 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth.models import Group
 from django.utils.html import format_html
+
 from django.utils.translation import gettext_lazy as _
 
 from .models import MaderaUser, Client, SalesPerson
@@ -40,15 +43,61 @@ class UserTypefilter(admin.SimpleListFilter):
             return SalesPerson.objects.all()
 
 
+class UserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = MaderaUser
+        fields = ('first_name', 'last_name', 'email', 'phone', 'address')
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = MaderaUser
+        fields = ('first_name', 'last_name', 'email', 'password')
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
+
+
 @admin.register(MaderaUser)
 class MaderaUserAdmin(UserAdmin):
     list_per_page = 20
     list_display = ('id_display', 'user_display', 'email_user_display', 'date_joined_display',
                     'address')
     fieldsets = (
+        (None, {'fields': ('email', 'password')}),
         ('Personal info',
             {'fields':
-                ('username', 'email', 'password', 'first_name', 'last_name', 'address')
+                ('first_name', 'last_name', 'address')
              }
          ),
         ('Professionnel',
@@ -63,6 +112,13 @@ class MaderaUserAdmin(UserAdmin):
             {'fields':
                 ('date_joined',)
              }
+         ),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
          ),
     )
 
@@ -116,3 +172,5 @@ class MaderaUserAdmin(UserAdmin):
         except AttributeError:
             return ''
     last_con_display.short_description = "Derniere connexion"
+
+admin.site.unregister(Group)
--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,31 +1,44 @@
-from django.contrib.auth.models import User
+import floppyforms.__future__ as forms
+
 from django.forms import ModelForm
 
 from .models import MaderaUser, SalesPerson, Client
 
+USER_FORM_FIELDS = ['first_name', 'last_name', 'email', 'phone']
+
 
 class UserForm(ModelForm):
 
-    class Meta:
-        model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
-
-
-class ProfileForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+        self.fields['email'].required = True
+        self.fields['phone'].required = True
 
     class Meta:
         model = MaderaUser
-        fields = ('location',)
+        fields = USER_FORM_FIELDS
+        widgets = {
+            'first_name': forms.TextInput(),
+            'last_name': forms.TextInput(),
+            'email': forms.TextInput(),
+            'phone': forms.TextInput()
+        }
+
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        return cleaned_data
 
 
-class SalesPersonForm(ModelForm):
+class SalesPersonForm(UserForm):
 
     class Meta:
         model = SalesPerson
         fields = ('workplace',)
 
 
-class ClientForm(ModelForm):
+class ClientForm(UserForm):
 
     class Meta:
         model = Client
