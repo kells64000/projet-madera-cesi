d007a09f35b7f98663ba53e1ec77c95c0ec4b4ee
kells <kelian.bousquet@laposte.net>
2019-03-12 12:59:34 +0100

    add maildev and settings mail dev

--- madera/settings.py
+++ madera/settings.py
@@ -224,13 +224,17 @@ USE_L10N = True
 USE_TZ = True
 
 # EMAIL
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = 'madera.societe@gmail.com'
-EMAIL_HOST_PASSWORD = 'maderacesi'
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
-EMAIL_SUBJECT_PREFIX = '[Devis]'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 465
+# EMAIL_HOST_USER = 'madera.societe@gmail.com'
+# EMAIL_HOST_PASSWORD = 'maderacesi'
+# EMAIL_USE_TLS = False
+# EMAIL_USE_SSL = True
+# EMAIL_SUBJECT_PREFIX = '[Devis]'
+
+# EMAIL DEV
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
